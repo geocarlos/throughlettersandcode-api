07415be62fa4bf75551d2b3eb5f94e923ee1f1e8
Geocarlos Alves <geowildcat@gmail.com>
2019-07-28 10:31:03 -0400

    Implemented security token code

---
+++ src/main/java/com/throughlettersandcode/config/property/ThroughLettersAndCodeApiProperty.java
@@ -0,0 +1,35 @@
+package com.throughlettersandcode.config.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("throughlettersandcode")
+public class ThroughLettersAndCodeApiProperty {
+	
+	private String allowedOrigin = "http://localhost:8000";
+	
+	private final Security security = new Security();
+	
+	public Security getSecurity() {
+		return security;
+	}
+	
+	public String getAllowedOrigin() {
+		return allowedOrigin;
+	}
+
+	public void setAllowedOrigin(String alloweString) {
+		this.allowedOrigin = alloweString;
+	}
+
+	public static class Security{
+		private boolean enableHttps;
+
+		public boolean isEnableHttps() {
+			return enableHttps;
+		}
+
+		public void setEnableHttps(boolean enableHttps) {
+			this.enableHttps = enableHttps;
+		}		
+	}
+}
