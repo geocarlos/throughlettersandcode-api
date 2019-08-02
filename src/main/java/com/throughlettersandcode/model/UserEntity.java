620e7ee70fd4778f1437b64f67426ccce1aab5ce
Geocarlos Alves <geowildcat@gmail.com>
2019-08-01 21:03:49 -0400

    Update User model for provided ID

--- src/main/java/com/throughlettersandcode/model/UserEntity.java
+++ src/main/java/com/throughlettersandcode/model/UserEntity.java
@@ -5,8 +5,6 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
@@ -17,7 +15,6 @@ import javax.persistence.Table;
 @Table(name = "user_entity")
 public class UserEntity {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String username;
