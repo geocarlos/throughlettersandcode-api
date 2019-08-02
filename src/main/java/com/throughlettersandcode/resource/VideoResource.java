127f5005db454890694828a1165ef7382a82fa8f
Geocarlos Alves <geowildcat@gmail.com>
2019-08-01 20:47:47 -0400

    Create PUT route for videos

--- src/main/java/com/throughlettersandcode/resource/VideoResource.java
+++ src/main/java/com/throughlettersandcode/resource/VideoResource.java
@@ -10,7 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.throughlettersandcode.event.ResourceCreatedEvent;
 import com.throughlettersandcode.model.Video;
 import com.throughlettersandcode.repository.VideoRepository;
+import com.throughlettersandcode.service.PublicationService;
 
 @RestController
 @RequestMapping("/videos")
 public class VideoResource {
 	@Autowired
 	private VideoRepository videoRepository;
+
+	@Autowired
+	private PublicationService publicationService;
 	
 	@Autowired
 	private ApplicationEventPublisher publisher;
@@ -41,4 +47,15 @@ public class VideoResource {
 		publisher.publishEvent(new ResourceCreatedEvent(this, response, savedVideo.getId()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(savedVideo);
 	}
+
+	@PutMapping("/{id}")
+	@PreAuthorize("hasAuthority('ROLE_CREATE_VIDEO')")
+	public ResponseEntity<Video> updateArticle(@PathVariable Long id, @RequestBody Video video) {
+		try {
+			Video savedVideo = publicationService.updateVideo(id, video);
+			return ResponseEntity.ok(savedVideo);
+		} catch (Exception e) {
+			return ResponseEntity.notFound().build();
+		}
+	} 
 }
